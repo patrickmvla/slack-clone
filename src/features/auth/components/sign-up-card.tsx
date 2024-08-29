b18602a3988223e07c58a1da7e1f214e44079d9e
patrick mvula <mvlapatrick@gmail.com>
2024-08-29 10:51:17 +0200

    login cards

---
+++ src/features/auth/components/sign-up-card.tsx
@@ -0,0 +1,98 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "@/features/auth/types";
+import { useState } from "react";
+
+interface Props {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignUpCard = ({ setState }: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  return (
+    <Card className="w-full h-full p-8 ">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Sign up to continue</CardTitle>
+        <CardDescription>
+          Use email or other services to continue
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Input
+            disabled={false}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirm password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            variant="outline"
+            onClick={() => {}}
+            disabled={false}
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle className="size-5 absolute top-2.5 left-2.5" />
+            Continue with Google
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => {}}
+            disabled={false}
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub className="size-5 absolute top-2.5 left-2.5" />
+            Continue with Github
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Already have an account?!{" "}
+          <span
+            className="text-sky-700 hover:underline cursor-pointer"
+            onClick={() => setState("signIn")}
+          >
+            Sign In
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
